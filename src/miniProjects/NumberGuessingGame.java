a41a2d7d3cac200f4e5f653ce4bbfd17d97a8bd3
smshamol16-star <smshamol16@gmail.com>
2026-05-02 20:50:42 +0600

    Update NumberGuessingGame.java-add Game over option

--- src/miniProjects/NumberGuessingGame.java
+++ src/miniProjects/NumberGuessingGame.java
@@ -4,38 +4,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessingGame {
-    public static void main(String[]args) {
+    public static void main(String[]args){
+
+                int guessNumber;
+                int randomNumber;
+                int attempt = 0;
+
+                Random random = new Random();
+                randomNumber = random.nextInt(1,100+1);
+
+                Scanner scanner = new Scanner(System.in);
+
+                do{
+                    attempt++;
+                    System.out.println("Guess a number among(1-100)");
+                    System.out.print("Enter the number that you have choosed:- ");
+                    guessNumber = scanner.nextInt();
 
 
-        int randomNumber;
-        int guess;
-        int attempt = 0;
+                    if (guessNumber>randomNumber){
+                        System.out.println("Too high");
+                    } else if (guessNumber<randomNumber) {
+                        System.out.println("Too low");
+                    }
+                    else {
+                        System.out.println("Correct! You got it in " +attempt+" attempts!");
+                    }
 
+                }while (guessNumber!=randomNumber && attempt<7);
+                if (guessNumber != randomNumber) {
+                    System.out.println("Game over! The number was " + randomNumber);
+                }
 
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        randomNumber = random.nextInt(1, 100 + 1);
-        do {
-            System.out.println("Chose a number between (1-100).");
+                scanner.close();
 
-            System.out.print("Enter the number that you have choosed:- ");
-            guess = scanner.nextInt();
-
-            if (guess<0){
-                System.out.println("Invalid input.");
-                continue;
             }
-            else if (guess > randomNumber) {
-                System.out.println("Too high.");
-            } else if (guess < randomNumber) {
-                System.out.println("Too low.");
-            } else {
-                System.out.println("You won.");
-                System.out.println("I have taken " + attempt + " attempt.");
-            }
-            attempt++;
-
-        } while (guess != randomNumber);
-
         }
-    }
+
+
