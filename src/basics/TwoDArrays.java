78ec65102a2def1e2653f62e91e7ddac8e2ad180
smshamol16-star <smshamol16@gmail.com>
2026-07-19 14:42:44 +0600

    Add TwoDArrays challange2.java

--- src/basics/TwoDArrays.java
+++ src/basics/TwoDArrays.java
@@ -7,7 +7,7 @@ public class TwoDArrays {
     public  static void main(String[]args){
 
         challange1();
-
+        challange2();
 
     }
     static void challange1(){
@@ -72,8 +72,44 @@ public class TwoDArrays {
     }
     static void challange2(){
 
+        System.out.print("How many classes?: ");
+        int totalClasses = scanner.nextInt();
+        scanner.nextLine();
 
+        String[][] students = new String[totalClasses][];
 
+        for (int i = 0; i < totalClasses; i++) {
+
+            System.out.print("Enter Class " + (i + 1) + " student numbers: ");
+            int totalStudents = scanner.nextInt();
+            scanner.nextLine();
+
+            students[i] = new String[totalStudents];
+
+            for (int j = 0; j < totalStudents; j++) {
+
+                System.out.print("Enter Student " + (j + 1) + " name: ");
+
+                students[i][j] = scanner.nextLine();
+
+            }
+
+        }
+
+        System.out.println("\n===== All Students =====");
+
+        for (int i = 0; i < totalClasses; i++) {
+
+            System.out.println("\nClass " + (i + 1) + ":");
+
+            for (int j = 0; j < students[i].length; j++) {
+
+                System.out.println(students[i][j]);
+
+            }
+        }
+
+        scanner.close();
     }
 
 
